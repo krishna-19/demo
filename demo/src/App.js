0d41541771a8f2957ac41b151b41327a5be9c003
krishna k <56662032+krishna-19@users.noreply.github.com>
2020-12-11 19:46:31 +0530

    Add files via upload

---
+++ demo/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    
+    return (
+      <div>
+      <div className="header"> </div>
+      <div className="main"> </div> 
+      <div className="footer"> </div>
+      </div>
+      
+       
+  );
+}
+
+export default App;
